fa21639c1c558db07e47596e5e51564fdfc5e63a
MeiyinPU <mei-yin.pu@oclock.school>
2024-01-11 22:00:14 +0100

    add hello world

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       {/* <Example count={count} setCount={setCount}/> */}
-
+     <h3>Hello world></h3>
       {/* Mettre un bouton pour mettre à jour count */}
       {/* Dans le composant Exo, afficher un console.log pour indiquer que count vient d'être modifié */}
      {/*  <button onClick={() => setCount(count + 1)}>Ajouter 1 {count}</button>
